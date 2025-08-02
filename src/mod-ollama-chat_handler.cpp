b22be0663bcb51c669e9d0b0b61944ad4465e308
Dustin Hendrickson <dustin@skillsgapp.com>
2025-08-02 15:07:18 -0700

    General Channel being weird

--- src/mod-ollama-chat_handler.cpp
+++ src/mod-ollama-chat_handler.cpp
@@ -891,7 +891,7 @@ void PlayerBotChatHandler::ProcessChat(Player* player, uint32_t /*type*/, uint32
                     if (cMgr)
                     {
                         Channel* targetChannel = cMgr->GetChannel(channelName, botPtr);
-                        if (targetChannel && targetChannel->GetPlayerFlags(botPtr->GetGUID()) != 0)
+                        if (targetChannel && targetChannel->IsOn(botPtr->GetGUID()))
                         {
                             targetChannel->Say(botPtr->GetGUID(), response, LANG_UNIVERSAL);
                             if(g_DebugEnabled)
